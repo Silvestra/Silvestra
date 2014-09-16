12c160a1fcf693d5ce214c56308709ae7ad14e45
Tadas <tadcka89@gmail.com>
2014-09-17 02:33:13 +0300

    Image gallery javascript.

--- Resources/public/js/libs/gallery.js
+++ Resources/public/js/libs/gallery.js
@@ -8,31 +8,17 @@
  */
 
 $.fn.mediaGallery = function () {
-    var $gallery = $(this);
+    var $self = $(this);
     var $imageUploadModal = new MediaImageUploadModal();
-    var $addImageButton = $('a#add-image-button');
 
-    $gallery.data('index', $gallery.find(':input').length);
+    $self.each(function () {
+        var $gallery = $(this);
+        var $addImageButton = $gallery.find('a#add-image-button:first');
 
-    $gallery.on('click', $addImageButton, function () {
-        $imageUploadModal.show();
+        $gallery.data('index', $gallery.find(':input').length);
+
+        $gallery.on('click', $addImageButton, function () {
+            $imageUploadModal.show($gallery);
+        });
     });
-
-    $imageUploadModal.getUploadButton().on('click', function () {
-        var $imageForm = $(addImageForm());
-        var $imageInput = $imageForm.find(':input[type=file]:first');
-
-        console.log($imageInput);
-    });
-
-    function addImageForm() {
-        var $prototype = $gallery.data('prototype');
-        var $index = $gallery.data('index');
-        var $newImageForm = $prototype.replace(/__name__/g, $index);
-
-        $gallery.data('index', $index + 1);
-        $gallery.find('div.images:first').prepend($newImageForm);
-
-        return $newImageForm;
-    }
 };
--- Resources/public/js/libs/imageUploadModal.js
+++ Resources/public/js/libs/imageUploadModal.js
@@ -8,20 +8,99 @@
  */
 
 function MediaImageUploadModal() {
-
     var $modal = $('div.image-upload-modal');
-    var $uploadButton = $modal.find('a.image-upload-button:first');
-    var $progressBar = $modal.find('div.progress-bar:first');
+    var $modalDialog = $modal.find('.modal-dialog:first');
+    var $uploadButton = $modal.find('span.image-upload-button:first');
+    var $dropZone = $modal.find('div.drop-zone:first');
+    var $modalDialogWidth;
 
-    this.show = function () {
+    this.show = function ($gallery) {
         $modal.modal('show');
+        $modalDialogWidth = $modalDialog.width();
+
+        $uploadButton.on('click', function () {
+            var $imageForm = $(addImageForm($gallery));
+            var $imageUploadFile = $imageForm.find(':input[type=file]:first');
+
+            $imageUploadFile.click();
+
+            $imageUploadFile.fileupload({
+                dataType: 'json',
+                autoUpload: false,
+                acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+                maxFileSize: 5000000,
+                previewMaxWidth: 300,
+                previewMaxHeight: 300,
+                previewCrop: false,
+
+                processalways: function (e, data) {
+                    var $file = data.files[data.index];
+                    var coordinates;
+                    var loadingImage = loadImage(
+                        $file,
+                        function (image) {
+                            $dropZone.html(image);
+
+                            var imageCanvas = $dropZone.find('img, canvas');
+
+                            imageCanvas.Jcrop({
+                                trueSize: [imageCanvas[0].width, imageCanvas[0].height],
+                                setSelect: [40, 40, imageCanvas[0].width - 40, imageCanvas[0].height - 40],
+                                onSelect: function (coords) {
+                                    coordinates = coords;
+                                },
+                                onRelease: function () {
+                                    coordinates = null;
+                                }
+                            });
+
+                            setModalDialogWidth(imageCanvas[0].width + 34);
+                        },
+                        {
+                            maxWidth: $dropZone.width(),
+                            maxHeight: $(window).height() - 300,
+                            canvas: true
+                        } // Options
+                    );
+
+                    if (!loadingImage) {
+                        $dropZone.html('<span>Your browser does not support the URL or FileReader API.</span>');
+                    }
+                }
+            });
+        });
     };
+
 
     this.getUploadButton = function () {
         return $uploadButton;
     };
 
-    this.getProgressBar = function () {
-        return $progressBar;
+    $modal.on('hidden.bs.modal', function () {
+        $uploadButton.unbind();
+        resetDropZone();
+        resetModalWidth();
+    });
+
+    var addImageForm = function ($gallery) {
+        var $index = $gallery.data('index');
+        var $newImageForm = $gallery.data('prototype').replace(/__name__/g, $index);
+
+        $gallery.data('index', $index + 1);
+        $gallery.find('div.images:first').prepend($newImageForm);
+
+        return $newImageForm;
+    };
+
+    var setModalDialogWidth = function ($width) {
+        $modalDialog.css('width', $width + 'px');
+    };
+
+    var resetDropZone = function () {
+        $dropZone.html('');
+    };
+
+    var resetModalWidth = function () {
+        setModalDialogWidth($modalDialogWidth);
     };
 }
