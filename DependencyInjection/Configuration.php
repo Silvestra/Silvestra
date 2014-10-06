732b1bd0a323d6f98ea7c6570c25dc35d5edfe68
Tadas <tadcka89@gmail.com>
2014-10-07 00:58:02 +0300

    Add seo metadata doctrine orm manager.

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Silvestra package.
+ *
+ * (c) Silvestra <tadcka89@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Silvestra\Bundle\SeoBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('silvestra_seo');
+
+
+        return $treeBuilder;
+    }
+}
