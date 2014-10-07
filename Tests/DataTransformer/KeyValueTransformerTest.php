562b0fda1b52b287dc89b7dc2340837fb93c3245
Tadas <tadcka89@gmail.com>
2014-10-08 01:42:10 +0300

    Initial tests.

---
+++ Tests/DataTransformer/KeyValueTransformerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Tadcka package.
+ *
+ * (c) Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Silvestra\Component\Form\Tests\DataTransformer;
+
+use Silvestra\Component\Form\DataTransformer\KeyValueTransformer;
+
+/**
+ * @author Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * @since 10/8/14 1:22 AM
+ */
+class KeyValueTransformerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var KeyValueTransformer
+     */
+    private $keyValueTransformer;
+}
---
+++ Tests/Type/KeyValueTypeTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Tadcka package.
+ *
+ * (c) Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Silvestra\Component\Form\Tests\Type;
+
+/**
+ * @author Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * @since 10/8/14 1:20 AM
+ */
+class KeyValueTypeTest extends \PHPUnit_Framework_TestCase
+{
+
+}
