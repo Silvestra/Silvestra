e279273f1c5c49f7d1e5940cdc2f1fe67b4ef2d2
Tadas <tadcka89@gmail.com>
2014-10-08 00:38:40 +0300

    Add key value form type.

---
+++ KeyValueArray.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the Silvestra package.
+ *
+ * (c) Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Silvestra\Component\Form;
+
+/**
+ * @author Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * @since 10/8/14 12:10 AM
+ */
+class KeyValueArray implements \ArrayAccess
+{
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * Constructor.
+     *
+     * @param array $data
+     */
+    public function __construct(array $data = array())
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetExists($offset)
+    {
+        return array_key_exists($offset, $this->data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetGet($offset)
+    {
+        return $this->data[$offset];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->data[$offset] = $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->data[$offset]);
+    }
+}
