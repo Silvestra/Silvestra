493d9c11cf928996f557e0d219f97176be5e4ad3
Justas Vitaitis <justas.vitaitis@nfq.lt>
2015-11-06 14:24:44 +0200

    Added silvestra_text configuration node_types

--- src/Silvestra/Bundle/TextBundle/DependencyInjection/Configuration.php
+++ src/Silvestra/Bundle/TextBundle/DependencyInjection/Configuration.php
@@ -11,6 +11,7 @@
 
 namespace Silvestra\Bundle\TextBundle\DependencyInjection;
 
+use Silvestra\Bundle\Text\NodeBundle\Model\TextNodeInterface;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -38,6 +39,9 @@ class Configuration implements ConfigurationInterface
                             ->end()
                         ->end()
                     ->end()
+                ->end()
+                ->arrayNode('node_types')->defaultValue(array(TextNodeInterface::NODE_TYPE))
+                    ->prototype('scalar')->cannotBeEmpty()
                 ->end()
             ->end();
 
